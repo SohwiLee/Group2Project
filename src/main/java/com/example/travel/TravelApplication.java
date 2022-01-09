40c16bb51eb6d9daff0bea7516bd6ca982e78de9
SH <sohwi.l.green@gmail.com>
2022-01-09 23:06:03 +0900

    servlet

--- src/main/java/com/example/travel/TravelApplication.java
+++ src/main/java/com/example/travel/TravelApplication.java
@@ -2,12 +2,17 @@ package com.example.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // test test
 @EnableJpaAuditing
 @SpringBootApplication
-public class TravelApplication {
+public class TravelApplication extends SpringBootServletInitializer {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(TravelApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TravelApplication.class, args);
